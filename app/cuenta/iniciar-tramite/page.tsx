96d0dffa8fb22933764ab58bc7e149be17da5422
jms2o <44822120+jms2o@users.noreply.github.com>
2026-08-22 22:37:43 -0700

    feat: add procedure start mode selector

--- app/cuenta/iniciar-tramite/page.tsx
+++ app/cuenta/iniciar-tramite/page.tsx
@@ -1,4 +1,7 @@
-import { ProcedureWizard } from "@/components/account/procedure-wizard";
+import { ProcedureStartExperience } from "@/components/account/procedure-start-experience";
 import { getClientWizardData } from "@/lib/db/client-portal";
 
-export default async function StartProcedurePage() { const data = await getClientWizardData(); return <div className="wizard-fullscreen"><ProcedureWizard {...data} /></div>; }
+export default async function StartProcedurePage() {
+  const data = await getClientWizardData();
+  return <div className="wizard-fullscreen"><ProcedureStartExperience {...data} /></div>;
+}
